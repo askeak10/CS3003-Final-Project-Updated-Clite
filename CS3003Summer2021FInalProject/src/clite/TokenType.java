6a404966d36ffdd23788242e9cdf229f5b123b91
askeak10 <71391359+askeak10@users.noreply.github.com>
2021-08-05 14:27:47 -0400

    Add files via upload

---
+++ CS3003Summer2021FInalProject/src/clite/TokenType.java
@@ -0,0 +1,9 @@
+// Sourced from https://github.com/NickStephens/Clite
+package clite;
+
+public enum TokenType {
+     Bool, Char, Else, False, Float, If, Int, Main, True, While, For, Void, Return, Print, Double, Eof, LeftBrace,
+     RightBrace, LeftBracket, RightBracket, LeftParen, RightParen, Semicolon, Comma, Assign, Equals, Less, LessEqual,
+     Greater, GreaterEqual, Not, NotEqual, Plus, Minus, Multiply, Divide, Modulo, And, Or, Identifier, IntLiteral,
+     FloatLiteral, CharLiteral, DoubleLiteral
+}
---
+++ CS3003Summer2021FInalProject/test.cpp
@@ -0,0 +1,21 @@
+double alpha;
+int beta;
+
+int main()
+{
+    int a, b, c;
+    double d, e;
+    beta = 5;
+    a = 5;
+    b = 2;
+    c = a % b;
+    d = c + e;
+    for (a > b)
+    {
+        for (a == b)
+        {
+            a = a + 1;
+            d = b % beta;
+        }
+    }
+}
